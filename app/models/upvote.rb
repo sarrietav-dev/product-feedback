6ff492d9f796874ac06cc9e860cf0df2f76e6ee0
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-02-13 19:00:59 -0500

    refactor: remove Turbo Stream broadcasting from Upvote model and clean up suggestion views

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -3,25 +3,4 @@ class Upvote < ApplicationRecord
 
   belongs_to :suggestion, counter_cache: true
   belongs_to :user
-
-  after_create_commit { broadcast_upvote_update }
-  after_destroy_commit { broadcast_upvote_update }
-
-  private
-
-  def broadcast_upvote_update
-    Turbo::StreamsChannel.broadcast_replace_to(
-      dom_id(suggestion),
-      target: dom_id(suggestion, "mobile"),
-      partial: "upvotes/upvote",
-      locals: {suggestion: suggestion, device: :mobile}
-    )
-
-    Turbo::StreamsChannel.broadcast_replace_to(
-      dom_id(suggestion),
-      target: dom_id(suggestion, "desktop"),
-      partial: "upvotes/upvote",
-      locals: {suggestion: suggestion, device: :desktop}
-    )
-  end
 end
