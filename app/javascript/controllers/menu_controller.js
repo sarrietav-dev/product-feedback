4a97437a0a3204fa16caeb27df988e3e067acabc
Sebastian Arrieta Villarreal <25210925+sarrietav-dev@users.noreply.github.com>
2025-02-17 10:11:31 -0500

    feat: implement toggle functionality for mobile menu with Stimulus controller

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,3 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
-export default class extends Controller {}
+export default class extends Controller {
+  static targets = ["menu", "button", "backdrop"];
+
+  toggle() {
+    this.buttonTarget.src = !this.menuTarget.classList.contains("hidden")
+      ? this.buttonTarget.dataset.hamburgerSrc
+      : this.buttonTarget.dataset.closeSrc;
+    this.menuTarget.classList.toggle("hidden");
+    this.backdropTarget.classList.toggle("hidden");
+  }
+}
